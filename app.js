624a05c4329c41020e72d0f37c284072f07c092f
cozy code <cozy.code@outlook.com>
2014-11-25 16:57:23 +0900

    add skeleton

---
+++ app.js
@@ -0,0 +1,48 @@
+var express=require('express');
+var morgan=require('morgan');   //for logging
+var url = require('url');
+
+var app=express();
+
+// init socket
+var server = require('http').Server(app);
+var io=require('socket.io')(server);
+
+// set template engine
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+
+// middleware
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+var server = app.listen(3000);
+
+app.get('/', function(req,res){
+    //console.log(url.parse(req.url,true));
+    console.log(req.query);
+
+
+    res.render('index',{title:"TITLE", socketURL: req.protocol + "://" + req.hostname + ":" + "3000"});
+});
+
+// init socket
+var io=require('socket.io')(server);
+
+// https://gist.github.com/arisetyo/5928974
+var messageArray=[{message:"Welcome!"}];
+
+io.on('connection',function(socket){
+    console.log('connected');
+    socket.emit('pushdata',messageArray);
+    socket.on('input',function(data){
+        console.log('input');
+        messageArray.push(data);
+        io.sockets.emit('pushdata',messageArray);
+    });
+    socket.on('disconnect',function(){
+        io.sockets.emit("user disconnected");
+    });
+});
+
+
